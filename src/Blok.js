3ea67bd4f677b1f4b6cb3057629769b844577808
Stefan Nieuwenburg~ <Nieuwenburg.stefan@gmail.com>
2026-01-20 15:47:15 +0100

    first page

---
+++ src/Blok.js
@@ -0,0 +1,45 @@
+
+<div class="grid-container full" bis_skin_checked="1">
+  <div class="grid-container" bis_skin_checked="1">
+    <div class="grid-x grid-padding-x align-center" bis_skin_checked="1">
+      <div class="cell small-12 medium-9 large-8 header-intro-wrapper" bis_skin_checked="1">
+
+        <div class="media-object stack-for-small" bis_skin_checked="1">
+
+        <div class="media-object-section" bis_skin_checked="1">
+               <div class="thumbnail" bis_skin_checked="1">
+              <div class="header-foto" bis_skin_checked="1">
+                <img
+                  width="375"
+                  height="375"
+                  src="https://edithsamuels.nl/wp-content/uploads/2020/06/edith-foto.jpg"
+                  class="attachment-post-thumbnail size-post-thumbnail wp-post-image"
+                  alt=""
+                  decoding="async"
+                  srcset="
+                    https://edithsamuels.nl/wp-content/uploads/2020/06/edith-foto.jpg 375w,
+                    https://edithsamuels.nl/wp-content/uploads/2020/06/edith-foto-300x300.jpg 300w,
+                    https://edithsamuels.nl/wp-content/uploads/2020/06/edith-foto-150x150.jpg 150w
+                  "
+                  sizes="(max-width: 375px) 100vw, 375px"
+                />
+              </div>
+               </div>
+          </div>
+
+          <div class="media-object-section main-section" bis_skin_checked="1">
+            <p class="edith-rond-blok">
+              Ik ben Edith, heldervoelend, -horend, -ziend en -wetend. Ik help je graag met persoonlijke en zakelijke vragen, geef je graag meer inzicht als je denkt vast te lopen. Mijn aanpak is nuchter en pragmatisch en ik kan je in een één op één gesprek sprongen vooruit helpen. Wat is je concrete vraag?
+            </p>
+
+            <p class="button-intro">
+              <a href="https://edithsamuels.nl/afspraak/" bis_skin_checked="1">Boek een afspraak</a>
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,8 +1,9 @@
+
 import vuurtoren from "./assets/vuurtorenwave.jpg"
 import edith from "./assets/edith.jpg"
-import weeg from "./assets/weeg.png"
-import oor from "./assets/oor.png"
-import compass from "./assets/compass.jpg"
+// import weeg from "./assets/weeg.png"
+// import oor from "./assets/oor.png"
+// import compass from "./assets/compass.jpg"
 import './Style.css'
 
 
@@ -39,22 +40,22 @@ const HomePage = () => {
       {/* Services */}
       <section className="py-12 px-6 grid md:grid-cols-3 gap-8 text-center">
         <div className="bg-white p-6 rounded shadow">
-           <img src={compass} alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
+           <img  alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
           <h3 className="text-xl font-semibold mb-2">Helder advies …</h3>
           <p className="text-[#8A8A8A]">Doordat ik kan wijzen in de juiste richting, krijg je helder welke keuzes je kunt maken. Dit kan helpen bij het oplossen van problemen. Mijn antwoord of advies is niet altijd wat je wilt, wel wat nodig is om verder te komen.</p>
         </div>
         <div className="bg-white p-6 rounded shadow">
-           <img src={weeg} alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
+           <img  alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
           <h3 className="text-xl font-semibold mb-2">Geen zweverige poespas…</h3>
           <p className="text-[#8A8A8A]">No no-nonsense met een pragmatische duidelijke aanpak. Een consult kan de nodige emotie oproepen, ik heb als doel om met jou de zaken tot de werkelijkheid terug te brengen.</p>
         </div>
         <div className="bg-white p-6 rounded shadow">
-           <img src={oor} alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
+           <img  alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
           <h3 className="text-xl font-semibold mb-2">Veilig plek …</h3>
           <p className="text-[#8A8A8A]">Veel mensen vinden het moeilijk om over persoonlijke emoties te praten. Je beslist zelf wat je wilt bespreken.</p>
         </div>
         <div className="bg-white p-6 rounded shadow">
-           <img src={oor} alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
+           <img  alt="Edith Image"  className="w-24 h-24  mx-auto mb-4" />
           <h3 className="text-xl font-semibold mb-2">Luisterend oor ...</h3>
           <p className="text-[#8A8A8A]">Ik luister en geef een duidelijk antwoord of advies op je vraag. Moeilijke situaties worden met respect besproken ook al zijn ze confronterend. Hierin kan ik ondersteunen.</p>
         </div>
