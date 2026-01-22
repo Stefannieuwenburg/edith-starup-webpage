76db9338c0fce35d153e7e20ead8de5714197661
Stefan Nieuwenburg~ <Nieuwenburg.stefan@gmail.com>
2026-01-22 13:02:30 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import HomePage from "./HomePage"
 import './Style.css'
 import Footer from "./components/Footer"
-import TrainingPage from "./pages/TrainingPage"
-import WorkshopPage from "./pages/WorkshopPage"
+import TrainingsPage from "./pages/TrainingPage"
+import WorkshopsPage from "./pages/WorkshopPage"
 
 function App() {
  
   return (
     <>
       <HomePage />
-      <TrainingPage />
-      <WorkshopPage />
+      <TrainingsPage />
+      <WorkshopsPage />
       <Footer />
     </>
   )
